3ce6e0174ca8107801f7dfd4726a2e05d2520fc6
logeek456 <loicjk@gmail.com>
2024-02-06 11:21:57 +0100

    Ajout de précisions pour le travail

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-
+Villes considérées comme des points
+On peut modif la hauteur des satellites, ils s'ajustent seuls pour rester au dessus de leur ville
--- spherical_satellites_repartition.py
+++ spherical_satellites_repartition.py
@@ -1,2 +1,6 @@
 import numpy as np
 import numba as nb
+
+
+def spherical_satellites_repartition(N_satellites, cities_coordinates, cities_weights, satellites_coordinates): 
+    return None
