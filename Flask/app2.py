6690944987ab2af67ccbdbd6a3d3bfb59ac8c456
tojanssen <tom.janssen@student.uclouvin.be>
2024-02-24 11:21:08 +0100

    C fonctionne. C'est la projection des satellites sur la sphere en respectant la deformation

--- Flask/app2.py
+++ Flask/app2.py
@@ -1,7 +1,21 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request, jsonify
 import folium, plotly 
 
 app = Flask(__name__)
+
+def calculer_pourcentage(lat, lng):
+    # Votre logique de calcul ici
+    return 1.6*lat
+
+
+@app.route('/calculer_pourcentage', methods=['POST'])
+def get_pourcentage():
+    data = request.json
+    lat = data['lat']
+    lng = data['lng']
+    pourcentage = calculer_pourcentage(lat, lng)
+    return jsonify({"pourcentage": pourcentage})
+
 
 @app.route('/simple-map')
 def simple_map():
--- Flask/static/script.js
+++ Flask/static/script.js
@@ -1,8 +1,68 @@
-function loadCustomScript() {
-    var draggableMarker = L.marker([51.505, -0.09], {draggable: true}).addTo(mymap);
+function loadCustomScript(mymap) {
+    var draggableMarker = L.marker([51.505, -2.09], {draggable: true}).addTo(mymap);
+    var circle = L.circle([51.505, -2.09], {
+        color: 'green', // Couleur du bord du cercle
+    fillColor: '#90EE90',
+        fillOpacity: 0.5,
+        radius: 500000
+    }).addTo(mymap);
+    
+    var draggableMarker2 = L.marker([40.508, -1.11], {draggable: true}).addTo(mymap);
+    var circle2 = L.circle([40.508, -1.11], {
+        color: 'green', // Couleur du bord du cercle
+    fillColor: '#90EE90',
+        fillOpacity: 0.5,
+        radius: 500000
+    }).addTo(mymap);
 
+    var draggableMarker3 = L.marker([31.505, -0.09], {draggable: true}).addTo(mymap);
+    var circle3 = L.circle([31.505, -0.09], {
+        color: 'green', // Couleur du bord du cercle
+    fillColor: '#90EE90',
+        fillOpacity: 0.5,
+        radius: 500000
+    }).addTo(mymap);
+    
     draggableMarker.on('dragend', function(e) {
         var position = draggableMarker.getLatLng();
+        circle.setLatLng(position);
         alert("Nouvelle position : " + position.lat + ", " + position.lng);
+        fetch('/calculer_pourcentage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({lat: position.lat, lng: position.lng})
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Mettre à jour l'information sur la carte avec le pourcentage reçu
+            var infoDiv = document.querySelector('.infoSurPopu');
+            if (infoDiv) {
+                infoDiv.innerHTML = 'Pourcentage de la population couverte : ' + data.pourcentage + '%';
+            }
+        })
+        .catch(error => {
+            console.error('Erreur lors de la requête AJAX :', error);
+        });
     });
+    draggableMarker2.on('dragend', function(e) {
+        var position2 = draggableMarker2.getLatLng();
+        circle2.setLatLng(position2);
+        alert("Nouvelle position 2 : " + position2.lat + ", " + position2.lng);
+    });
+    draggableMarker3.on('dragend', function(e) {
+        var position3 = draggableMarker3.getLatLng();
+        circle3.setLatLng(position3);
+        alert("Nouvelle position 3 : " + position3.lat + ", " + position3.lng);
+    });
+
+
+    var infoSurPopu = L.control({position: 'topright'});
+    infoSurPopu.onAdd = function (mymap) {
+        var div = L.DomUtil.create('div', 'infoSurPopu');
+        div.innerHTML = 'Pourcentage de la population couverte :</h4>' + '80%';
+        return div;
+    };
+    infoSurPopu.addTo(mymap);
 }
