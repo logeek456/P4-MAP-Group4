77b4396f768b2d38e683a2279a08cc337ec908e4
logeek456 <loicjk@gmail.com>
2024-03-05 10:24:58 +0100

    On a la Terre !!

--- sphere.py
+++ sphere.py
@@ -1,22 +1,34 @@
 import numpy as np
 import numba as nb
 from mayavi import mlab
+from tvtk.api import tvtk
+
+def auto_sphere(image_file):
+    # create a figure window (and scene)
+    fig = mlab.figure(size=(600, 600))
+
+    # load and map the texture
+    img = tvtk.JPEGReader()
+    img.file_name = image_file
+    texture = tvtk.Texture(input_connection=img.output_port, interpolate=1)
+    # (interpolate for a less raster appearance when zoomed in)
+
+    # use a TexturedSphereSource, a.k.a. getting our hands dirty
+    R = 1
+    Nrad = 180
+
+    # create the sphere source with a given radius and angular resolution
+    sphere = tvtk.TexturedSphereSource(radius=R, theta_resolution=Nrad,
+                                       phi_resolution=Nrad)
+
+    # assemble rest of the pipeline, assign texture    
+    sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
+    sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
+    fig.scene.add_actor(sphere_actor)
 
 
-def generate_topography():
-    file = open("topography_data.txt", "w")
-    for i in range(100):
-        for j in range(50):
-            file.write(str(np.random.randint(0,8000)) + " ")
-        file.write("\n")
-    file.close()
-
-generate_topography()
-
-#Generate a 100x50 .txt file for the topography. Needs to have an ocean (defined by height 0), a couple islands (height > 10) and a continent (height > 10) with a few mountains (height > 2000). Max height is 8000, and it should be continuous
-
-
-
+auto_sphere('earth.jpg')
+mlab.show()
 
 
 
